936249b7e15b1748eddb486da5382e11d303a35c
Brian Leung <bleung@navicat.com>
2020-10-21 15:55:39 +0800

    socket client for loop

--- myserver/MySocketClient.java
+++ myserver/MySocketClient.java
@@ -13,31 +13,35 @@ public class MySocketClient {
 		Socket connection;
 //		InputStream in;
 //		OutputStream out;
-		try {
-			connection = new Socket("localhost", 50505);
-			PrintWriter writerObj = new PrintWriter(connection.getOutputStream());
-			writerObj.write("GET /index.html HTTP/1.1\r\n");
-			writerObj.write("quit\r\n");
-			writerObj.flush();
 
-			InputStream input = connection.getInputStream();
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-//			
-//			String line;
-//			while ((line = reader.readLine()) != null ) {
-//				System.out.println(line);
-//			}
-			int data = input.read();
-			while(data != -1){
-			    System.out.print((char)data);
-			    data = input.read();
-			}
-			connection.close();
-//			connection.close();
-		} catch (Exception e) {
-			System.out.println("Failed to connect socket 50505.");
-		}
+		while (true) {
+			try {
+				connection = new Socket("localhost", 50505);
+				PrintWriter writerObj = new PrintWriter(connection.getOutputStream());
+				writerObj.write("GET /index.html HTTP/1.1\r\n");
+				writerObj.write("quit\r\n");
+				writerObj.flush();
 		
+				InputStream input = connection.getInputStream();
+		//		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		//		
+		//		String line;
+		//		while ((line = reader.readLine()) != null ) {
+		//			System.out.println(line);
+		//		}
+				int data = input.read();
+				while(data != -1){
+				    System.out.print((char)data);
+				    data = input.read();
+				}
+				connection.close();
+				Thread.sleep(1000);
+		//		connection.close();
+			} catch (Exception e) {
+				System.out.println("Failed to connect socket 50505.");
+			}
+
+		}	
 	}
 	
 }
