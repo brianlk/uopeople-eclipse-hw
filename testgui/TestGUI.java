741ef17dbde3d1db87245243147c10c5a31f4a3a
Brian Leung <brian.leungkp@gmail.com>
2020-10-31 13:06:51 +0800

    Simple Jframe window

--- testgui/TestGUI.java
+++ testgui/TestGUI.java
@@ -1,5 +1,13 @@
 package testgui;
 
-public class TestGUI {
+import java.awt.*;
+import javax.swing.*;
 
+public class TestGUI {
+	public static void main(String[] args) {
+		JFrame window = new JFrame("GUI");
+		window.setSize(250,100);
+		window.setLocation(0,0);
+		window.setVisible(true);
+	}
 }
