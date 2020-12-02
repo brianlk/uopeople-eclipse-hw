c2cc0398b5793f4543f5ccba1e51f3d54bcbd6db
Brian Leung <brian.leungkp@gmail.com>
2020-12-02 13:55:01 +0800

    cs3303 unit3 create linked list

--- cs3303unit3/CreateLinkedList.java
+++ cs3303unit3/CreateLinkedList.java
@@ -2,25 +2,39 @@ package cs3303unit3;
 
 public class CreateLinkedList {
 	public static void main(String[] args) {
-		Node link, plink;
+		Node plink = null;
+		Node link = null;
 	    // root will be the beginning of the linked list
-		Node root = new Node(5);
-	    // each additional node will link to preceeding one
-		link = new Node(1);
-	    link.ptr = root;
-	    plink = link;
-
-	    link = new Node(8);
-	    link.ptr = plink;
-	    plink = link;
-
-	    link = new Node(6);
-	    link.ptr = plink;
-	    plink = link;
-
-	    link = new Node(3);
-	    link.ptr = plink;
-	    plink = link;
+		int[] intArray = new int[] {2, 1, 0};
+		
+		for (int i=0; i < intArray.length; i++) {
+//			System.out.println(intArray[i]);
+			if (i == 0) {
+				Node root = new Node(intArray[i]);
+				plink = root;
+			} else {
+				link = new Node(intArray[i]);
+				link.ptr = plink;
+				plink = link;
+			}
+		}
+//		Node root = new Node(5);
+//	    // each additional node will link to preceeding one
+//		link = new Node(1);
+//	    link.ptr = root;
+//	    plink = link;
+//
+//	    link = new Node(8);
+//	    link.ptr = plink;
+//	    plink = link;
+//
+//	    link = new Node(6);
+//	    link.ptr = plink;
+//	    plink = link;
+//
+//	    link = new Node(3);
+//	    link.ptr = plink;
+//	    plink = link;
 	   
 	    // Move through the list and print out each value
 	    printList(link);
