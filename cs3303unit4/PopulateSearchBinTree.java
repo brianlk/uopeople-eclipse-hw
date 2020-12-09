c59cfbdbb86ed4cf7dbb86c715c041d19ab11165
brian <a@a.com>
2020-12-09 15:46:20 +0800

    Build binary tree structure

--- cs3303unit4/PopulateSearchBinTree.java
+++ cs3303unit4/PopulateSearchBinTree.java
@@ -18,10 +18,37 @@ public class PopulateSearchBinTree {
 		root = null;
 	}
 	
-	void insert(int key) {
+	void insert(Node node, int key) {
 		if (root == null) {
-			root = new Node(key);
+			root = new Node(50);
+			return;
 		}
+		if (key < node.value) {
+			if (node.left == null) {
+				node.left = new Node(key);
+			} else {
+				insert(node.left, key);
+			}
+		} else {
+			if (node.right == null) {
+				node.right = new Node(key);
+			} else {
+				insert(node.right, key);
+			}
+		}
+		return;
+		
+	}
+	
+	public static void main(String[] args) {
+		PopulateSearchBinTree tree = new PopulateSearchBinTree();
+		tree.insert(tree.root, 50);
+		tree.insert(tree.root, 40);
+		tree.insert(tree.root, 60);
+		tree.insert(tree.root, 10);
+		tree.insert(tree.root, 81);
+		tree.insert(tree.root, 71);
+		System.out.println(tree.root.right.right.left.value);
 	}
 
 }
