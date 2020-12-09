bc155ab9b4b8f444d45fd35086adcbf872c919c4
brian <a@a.com>
2020-12-09 19:14:25 +0800

    Fix binary tree search

--- cs3303unit4/PopulateSearchBinTree.java
+++ cs3303unit4/PopulateSearchBinTree.java
@@ -2,17 +2,19 @@ package cs3303unit4;
 
 import java.util.*;
 
-public class PopulateSearchBinTree {
-	// Declare the node class
-	class Node {
-		int value;
-		Node left, right;	
-		public Node(int input) {
-			value = input;
-			left = right = null;
-		}
+// Declare the node class
+class Node {
+	int value;
+	Node left, right;	
+	public Node(int input) {
+		value = input;
+		left = right = null;
 	}
-	
+}
+
+
+public class PopulateSearchBinTree {
+
 	// Declare the root
 	Node root;
 	int searchValue;
@@ -58,15 +60,15 @@ public class PopulateSearchBinTree {
 	
 	public static void main(String[] args) {
 		PopulateSearchBinTree tree = new PopulateSearchBinTree();
-		System.out.print("Integers to add to binary search tree: ");
-		Scanner in = new Scanner(System.in);
-		String input = in.nextLine();
-		String[] inputArr = input.replaceAll("\\s+", "").split(",");
+		System.out.println("Integers to add to binary search tree: ");
+		int[] inputArr = {10, 5, 12, 3, 1, 13, 7, 2, 4, 14, 9, 8, 6, 11};
+
 		for (int i=0; i < inputArr.length; i++) {
-			int x = Integer.parseInt(inputArr[i]);
-			tree.insert(tree.root, x);
+			System.out.println("insert integer: " + inputArr[i]);
+			tree.insert(tree.root, inputArr[i]);
 		}
 		System.out.println("Search an integer: ");
+		Scanner in = new Scanner(System.in);
 		int num = in.nextInt();
 		tree.search(tree.root, num);
 		in.close();
