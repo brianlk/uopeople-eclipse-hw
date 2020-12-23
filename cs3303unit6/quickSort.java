20c2d75d03a0fd3bc0caf622002f85a39371af00
Brian Leung <brian.leungkp@gmail.com>
2020-12-23 17:02:09 +0800

    Quick sort implementation

---
+++ cs3303unit6/quickSort.java
@@ -0,0 +1,16 @@
+package cs3303unit6;
+
+public class quickSort {
+
+	public static int[] swap(int[] array) {
+		
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		int array[] = {12,9,4,99,120,1,3,10,23,45,75,69,31,88,101,14,29,91,2,0,77};
+		swap(array);
+		
+	}
+	
+}
