f2e540858de4755739b7891b0e663bf128378d83
Brian Leung <brian.leungkp@gmail.com>
2021-03-23 22:52:15 +0800

    test package

---
+++ runProgram.java
@@ -0,0 +1,10 @@
+import testPackage.folder1.*;
+
+
+public class runProgram {
+
+	public static void main(String[] args) {
+		testPackage x = new testPackage();
+		System.out.println(x);
+	}
+}
---
+++ testPackage/folder1/testPackage.java
@@ -0,0 +1,8 @@
+package testPackage.folder1;
+
+public class testPackage {
+
+	public testPackage() {
+		System.out.println("xxxxxxxxxx");
+	}
+}
